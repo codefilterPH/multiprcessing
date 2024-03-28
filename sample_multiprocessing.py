0ee49a59448e0aa6cbbe027ede2a6338ba398fb6
codefilterPH <97372103+codefilterPH@users.noreply.github.com>
2024-03-28 15:43:42 +0800

    Update sample_multiprocessing.py

--- sample_multiprocessing.py
+++ sample_multiprocessing.py
@@ -49,17 +49,34 @@ def skill_data():
         },
     ]
 
-def process_skill_data(self, data):
+def process_skill_data(data):
     print(data)
 
-def process(self):
+def process():
     try:
         # Use multiprocessing Pool to parallelize the processing of skill data
         with multiprocessing.Pool() as pool:
-            pool.map(self.process_skill_data, self.skill_data())
+            pool.map(process_skill_data, skill_data())
 
         print("Data import successful.")
     except Exception as e:
         print(f"Error during data import: {str(e)}")
 
 
+if __name__ == "__main__":
+    process()
+
+    # OUTPUT 
+    """
+    D:\PycharmProjects\skaty\venv\Scripts\python.exe D:\PycharmProjects\skaty\exam\scripts\sample_multiprocessing.py 
+    {'skill': '3', 'description': '', 'exam_total_items': 50, 'reviewer_total_items': 150, 'duration_minutes': 0, 'duration_hours': 1}
+    {'skill': '5', 'description': '', 'exam_total_items': 75, 'reviewer_total_items': 175, 'duration_minutes': 30, 'duration_hours': 1}
+    {'skill': '7', 'description': '', 'exam_total_items': 100, 'reviewer_total_items': 200, 'duration_minutes': 0, 'duration_hours': 2}
+    {'skill': '9', 'description': '', 'exam_total_items': 100, 'reviewer_total_items': 200, 'duration_minutes': 0, 'duration_hours': 2}
+    {'skill': 'AFSC 10070/10090', 'description': 'First Sgt/Sgt Major AFSC', 'exam_total_items': 50, 'reviewer_total_items': 150, 'duration_minutes': 0, 'duration_hours': 1}
+    Data import successful.
+    
+    Process finished with exit code 0
+    """
+    # End of Terminal Input
+    
